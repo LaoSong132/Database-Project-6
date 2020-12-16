e2a133cee36f1be69102506850063482989f478b
bking52 <46727146+bking52@users.noreply.github.com>
2020-12-15 21:26:37 -0500

    Update AdvancedView1.sql

--- AdvancedView1.sql
+++ AdvancedView1.sql
@@ -1,10 +1,11 @@
-SELECT restaurant_review.restaurant_id, star_rating, description, date, schedule
+SELECT restaurant_review.restaurant_id, restaurant_name, (star_rating) AS `Average Rating`, restaurant.schedule, location
 FROM restaurant_review
 INNER JOIN restaurant
 ON restaurant_review.restaurant_id = restaurant.restaurant_id
 WHERE restaurant.restaurant_id IN (
     SELECT restaurant_id 
     FROM restaurant
-    WHERE schedule LIKE "%10pm")
+    WHERE schedule LIKE "9AM%")
 AND star_rating >= 3
-ORDER BY star_rating DESC;
+GROUP BY restaurant_id
+ORDER BY AVG(star_rating) DESC;
